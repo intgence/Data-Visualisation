03ee2c65ca818d8048ba0f092c76920b401306c2
intgence <palosibasis2000@outlook.com>
2020-04-12 13:43:31 +0530

    Changes in Population map style

--- Population/world_population.py
+++ Population/world_population.py
@@ -1,6 +1,7 @@
 import json
 
 import pygal
+from pygal.style import RotateStyle as RS, LightColorizedStyle as LCS
 
 from country_codes import get_country_code
 
@@ -31,7 +32,8 @@ for cc,pop in cc_populations.items():
 
 print(len(cc_pops_1),len(cc_pops_2),len(cc_pops_3))
 
-wm = pygal.maps.world.World()
+wm_style = RS('#336699',base_style=LCS)
+wm = pygal.maps.world.World(style=wm_style)
 wm.title = 'World Population in 2010, by Country'
 wm.add('0-10m', cc_pops_1)
 wm.add('10m-1bn',cc_pops_2)
